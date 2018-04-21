4ae5a93d445ea1818dedbb49f5a92ce01d83c039
Christoph Saxe <christoph_saxe@homedepot.com>
2018-04-20 22:12:09 -0700

    fill in rendering

--- src/components/organisms/forms/KeywordSearchForm/KeywordSearchForm.js
+++ src/components/organisms/forms/KeywordSearchForm/KeywordSearchForm.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import uid from 'uid';
 import './KeywordSearchForm.scss';
+import TextInput from '../../../atoms/TextInput/TextInput';
+import Label from '../../../atoms/Label/Label';
+import Button from '../../../atoms/Button/Button';
 import classnames from 'classnames';
 
 export const KeywordSearchForm = props => {
   const {
     variant,
     tagName,
+    identifier,
     extraClasses,
+    labelText,
+    inputAttr,
     children,
     ...attr,
   } = props;
 
   const Tag = tagName;
 
-  const className = classnames([
+  const inputId = identifier || uid();
+
+  const blockClassName = classnames([
     'KeywordSearchForm',
     `KeywordSearchForm-${variant}`,
     ...extraClasses,
   ]);
 
   return (
-    <Tag className={className} {...attr}>
-      {children}
+    <Tag className={blockClassName} {...attr}>
+      <div className="KeywordSearchForm__description">
+        <Label htmlFor={inputId} extraClasses={["KeywordSearchForm__label"]}>{labelText}</Label>
+      </div>
+      <div className="KeywordSearchForm__field">
+        <TextInput extraClasses={["KeywordSearhForm__input"]} {...inputAttr} type="search" />
+      </div>
+      <div className="KeywordSearchForm__control">
+        <Button extraClasses={["KeywordSearchForm__button"]}>Search</Button>
+      </div>
+      <div className="KeywordSearchForm__content">
+        {children}
+      </div>
     </Tag>
   );
 };
@@ -32,6 +52,7 @@ export default KeywordSearchForm;
 KeywordSearchForm.propTypes = {
   variant: PropTypes.oneOf(['default']),
   tagName: PropTypes.string,
+  labelText: PropTypes.string,
   extraClasses: PropTypes.array,
   children: PropTypes.node,
 }
@@ -39,5 +60,6 @@ KeywordSearchForm.propTypes = {
 KeywordSearchForm.defaultProps = {
   variant: 'default',
   tagName: 'div',
+  labelText: 'Search',
   extraClasses: [],
 }
